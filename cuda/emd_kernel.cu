4185a9178884bd411fb322213bf1fd84b7db222a
Cihan Öngün <37138861+cihanongun@users.noreply.github.com>
2020-07-06 12:56:44 +0300

    Replaced the deprecated AT_CHECK with TORCH_CHECK

--- cuda/emd_kernel.cu
+++ cuda/emd_kernel.cu
@@ -13,8 +13,8 @@
 #include <ATen/cuda/CUDAApplyUtils.cuh>  // at::cuda::getApplyGrid
 #include <THC/THC.h>
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 
